8c7220d8dbb52528280083db300cf17aa3a0ee38
supersett <nolang3748@naver.com>
2022-08-12 15:39:55 +0900

    feat : login permissions

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,18 @@ from django.urls import path,include
 from accounts import views
 
 urlpatterns = [
+    #Mypage
+    path('mypage/',views.MypageUserDetailAPIView.as_view(),name='MypageUserDetailAPIView'),
+    
+    #Info
+    path('info/',views.InfoUserDetailAPIView.as_view(),name='InfoUserDetailAPIView'),
+    
+    #소셜로그인(구글)
     path('google/login', views.google_login, name='google_login'),
     path('google/callback/', views.google_callback,      name='google_callback'),  
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
     
+    #소셜로그인(카카오)
     path('kakao/login/', views.kakao_login, name='kakao_login'),
     path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
     path('kakao/login/finish/', views.KakaoLogin.as_view(),
