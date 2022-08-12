b47aa6bfc1af3467ceda035863a7e870e1568c41
supersett <nolang3748@naver.com>
2022-08-12 20:54:13 +0900

    feat : user_mail_auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     #Info
     path('info/',views.InfoUserDetailAPIView.as_view(),name='InfoUserDetailAPIView'),
     
+    #메일인증요청
+    path('mail/auth/',views.MypageSchoolMailAuth.as_view(),name='MypageSchoolMailAuth'),
+    
+    #계정활성화
+    path('activate/<str:uidb64>/<str:token>',views.ActivateView.as_view(),name='ActivateView'),
+    
+    
     #소셜로그인(구글)
     path('google/login', views.google_login, name='google_login'),
     path('google/callback/', views.google_callback,      name='google_callback'),  
