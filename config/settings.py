8e78a760e0900295e7aacbd6c00744aa8840ea29
supersett <nolang3748@naver.com>
2022-08-16 23:19:35 +0900

    docs : settings > add CORS_ORIGIN_WHITELIST,CORS_ALLOW_CREDENTIALS

--- config/settings.py
+++ config/settings.py
@@ -200,6 +200,12 @@ STATE = os.environ.get("STATE")
 
 #CORS configuration
 CORS_ORIGIN_ALLOW_ALL= True
+CORS_ORIGIN_WHITELIST=[
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',                   
+                       ]
+
+CORS_ALLOW_CREDENTIALS=True
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
