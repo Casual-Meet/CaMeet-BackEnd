15e21802ac89f77db9561a3579125c0bc16e890b
supersett <nolang3748@naver.com>
2022-08-17 02:18:29 +0900

    docs : settings.py

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ def get_secret(setting,secrets=secrets):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',
                  '.ap-northeast-2.compute.amazonaws.com',
