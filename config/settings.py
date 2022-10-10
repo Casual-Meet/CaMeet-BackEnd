727f852b862da8aca2230058ae935ff08b8d8236
supersett <nolang3748@naver.com>
2022-10-10 21:37:41 +0900

    docs : error fix

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
